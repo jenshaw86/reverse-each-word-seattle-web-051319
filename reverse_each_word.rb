01cf8a95b33f3c11ae2d7e2473628349df506307
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-06 06:11:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(str)
-  arr = str.split(" ")
-  reversed_words = arr.collect do |word|
+  words = str.split(" ")
+  reversed_words = words.collect do |word|
     word.reverse
   end
   reversed_words.join(" ")
